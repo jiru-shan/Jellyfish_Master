7fb7aac1a39c06f0a30b3d4098a0d52ff09c39d5
linryan888 <linryan888@gmail.com>
2022-01-17 19:54:44 -0800

    Update EvenLessScuffedAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvenLessScuffedAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvenLessScuffedAuton.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
