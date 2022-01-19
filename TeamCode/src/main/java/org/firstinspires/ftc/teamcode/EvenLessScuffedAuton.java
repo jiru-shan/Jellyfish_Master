5cfd15dff1db955cb2bc2d29e2f43ceb2bf0e09c
linryan888 <linryan888@gmail.com>
2022-01-18 19:16:58 -0800

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvenLessScuffedAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvenLessScuffedAuton.java
@@ -2,8 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
@@ -11,16 +14,50 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+
 public class EvenLessScuffedAuton extends LinearOpMode
 {
     DcMotorEx test;
     private DistanceSensor sensorRange1, sensorRange2;
 
-    DcMotorEx leftFront, leftRear, rightRear, rightFront;
+    DcMotorEx leftFront, leftBack, rightBack, rightFront;
+    DcMotor lift_front, lift_back, leftIntake, rightIntake;
+    CRServo carousel;
+    Servo d_open;
+    Servo d_coverLeft;
+    Servo d_coverRight;
+    Servo d_bendLeft;
+    Servo d_bendRight;
+    Servo i_topLeft;
+    Servo i_bottomLeft;
+    Servo i_topRight;
+    Servo i_bottomRight;
 
     @Override
     public void runOpMode() throws InterruptedException
     {
+        // Motors
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        lift_front = hardwareMap.dcMotor.get("lift_front");
+        lift_back = hardwareMap.dcMotor.get("lift_back");
+        leftIntake = hardwareMap.dcMotor.get("leftIntake");
+        rightIntake = hardwareMap.dcMotor.get("rightIntake");
+
+        // Servos
+        d_open = hardwareMap.servo.get("d_open");
+        d_coverLeft = hardwareMap.servo.get("d_coverLeft");
+        d_coverRight = hardwareMap.servo.get("d_coverRight");
+        d_bendLeft = hardwareMap.servo.get("d_bendLeft");
+        d_bendRight = hardwareMap.servo.get("d_bendRight");
+        i_topLeft = hardwareMap.servo.get("i_topLeft");
+        i_bottomLeft = hardwareMap.servo.get("i_bottomLeft");
+        i_topRight = hardwareMap.servo.get("i_topRight");
+        i_bottomRight = hardwareMap.servo.get("i_bottomRight");
+        carousel = hardwareMap.crservo.get("carousel");
+
         test.getCurrent(CurrentUnit.AMPS);
         sensorRange1 = hardwareMap.get(DistanceSensor.class, "sensor_range");
         sensorRange2 = hardwareMap.get(DistanceSensor.class, "sensor_range");
@@ -57,5 +94,45 @@ public class EvenLessScuffedAuton extends LinearOpMode
         end = leftFront.getCurrentPosition();
         int distance = end - start;
         return distance;
+    }
+
+    public void lift(){
+        lift_front.setTargetPosition(-alliance_targetTipped);
+        lift_back.setTargetPosition(-alliance_targetTipped);
+        lift_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);   // Move to deposit position
+        lift_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift_front.setPower(-1.0);
+        lift_back.setPower(-1.0);
+
+        Thread.sleep(1000);
+
+        // Lift up deposit
+        d_bendLeft.setPosition(d_maxRange_bendLeft);
+        d_bendRight.setPosition(d_maxRange_bendRight);
+
+        // Open to deposit in top level of alliance hub
+        d_open.setPosition(d_open_top);
+
+        Thread.sleep(500);
+
+        // Close & bend down deposit
+        d_open.setPosition(d_open_minRange);
+        d_bendLeft.setPosition(d_minRange_bendLeft);
+        d_bendRight.setPosition(d_minRange_bendRight);
+
+        Thread.sleep(1000);
+
+        // Retract arm to original position
+        lift_front.setTargetPosition(0);
+        lift_back.setTargetPosition(0);
+        lift_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);   // Move to deposit position
+        lift_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift_front.setPower(1.0);
+        lift_back.setPower(1.0);
+
+        Thread.sleep(1000);
+
+        lift_front.setPower(0);
+        lift_back.setPower(0);
     }
 }
