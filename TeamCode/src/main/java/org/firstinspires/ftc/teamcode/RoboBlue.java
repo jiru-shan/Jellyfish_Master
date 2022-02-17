ff0371c300a8214470ce7f5d61a9e88ef4573424
Pompompurin <daphanie168@gmail.com>
2022-02-16 23:56:05 -0800

    Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboBlue.java
@@ -103,6 +103,7 @@ public class RoboBlue extends LinearOpMode {
     }
 
     enum LiftState {
+
         LS_EXTENDING,
         LS_EXTENDING_BALANCED,
         LS_EXTENDING_TIPPED,
@@ -119,7 +120,9 @@ public class RoboBlue extends LinearOpMode {
         LS_TARGET_FAR,
         LS_DEPOSITING,
         LS_RELEASED,
-        LS_RETRACTING
+        LS_RETRACTING,
+        LS_IDLE,
+        LS_RESET
     }
 
     enum LiftHand {
@@ -187,19 +190,19 @@ public class RoboBlue extends LinearOpMode {
         double i_maxRange_topLeft = 0.92;
         double i_minRange_bottomLeft = 0.90;
         double i_maxRange_bottomLeft = 0.13;
-        double i_minRange_topRight = 0.87;
-        double i_maxRange_topRight = 0.16;
-        double i_minRange_bottomRight = 0.16;
-        double i_maxRange_bottomRight = 0.87;
+        double i_minRange_topRight = 0.92;
+        double i_maxRange_topRight = 0.12;
+        double i_minRange_bottomRight = 0.12;
+        double i_maxRange_bottomRight = 0.92;
 
         // Carousel
         double maxSpinPower = 0.5;
 
         // Factor
         double normalSpeed = 1.0;
-        int TARGET_TIPPED = 420;
-        int TARGET_BALANCED = 380;
-        int TARGET_MIDDLE = 550;
+        int TARGET_TIPPED = 250;
+        int TARGET_BALANCED = 230;
+        int TARGET_MIDDLE = 210;
         int TARGET_NEAR = 80;
         int TARGET_CENTER = 120;
         int TARGET_FAR = 160;
@@ -432,7 +435,7 @@ public class RoboBlue extends LinearOpMode {
 
                     } else if (intakeHand == IntakeHand.IH_RIGHT) {
 
-                        if (rightIntakeTimer.milliseconds() > 800) {
+                        if (rightIntakeTimer.milliseconds() > 600) {
 
                             rightIntake.setPower(highSweepPower);
 
@@ -506,186 +509,204 @@ public class RoboBlue extends LinearOpMode {
             }
 
 
-//            /** Lift **/
-//
-//            // Motor tick count is equal to 384.5
-//
-//            switch (liftState) {
-//                case LS_EXTENDING:
-//
-//                    // Check if button was pressed
-//                    if (gamepad2.dpad_up) {
-//
-//                        liftHand = LiftHand.LH_TIPPED;
-//
-//                        // Run lift
-//                        liftLeft.setTargetPosition(TARGET_TIPPED);
-//                        liftRight.setTargetPosition(TARGET_TIPPED);
-//                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftLeft.setPower(-1.0);
-//                        liftRight.setPower(-1.0);
-//
-//                        liftState = LiftState.LS_TARGET;
-//
-//                    } else if (gamepad2.dpad_left) {
-//
-//                        liftHand = liftHand.LH_BALANCED;
-//
-//                        // Run lift
-//                        liftLeft.setTargetPosition(TARGET_BALANCED);
-//                        liftRight.setTargetPosition(TARGET_BALANCED);
-//                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftLeft.setPower(-1.0);
-//                        liftRight.setPower(-1.0);
-//
-//                        liftState = LiftState.LS_TARGET;
-//
-//                    } else if (gamepad2.y) {
-//
-//                        liftHand = LiftHand.LH_FAR;
-//
-//                        // Run lift
-//                        liftLeft.setTargetPosition(TARGET_FAR);
-//                        liftRight.setTargetPosition(TARGET_FAR);
-//                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftLeft.setPower(-1.0);
-//                        liftRight.setPower(-1.0);
-//
-//                        liftState = LiftState.LS_TARGET;
-//
-//                    } else if (gamepad2.b) {
-//
-//                        liftHand = LiftHand.LH_CENTER;
-//
-//                        // Run lift
-//                        liftLeft.setTargetPosition(TARGET_CENTER);
-//                        liftRight.setTargetPosition(TARGET_CENTER);
-//                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftLeft.setPower(-1.0);
-//                        liftRight.setPower(-1.0);
-//
-//                        liftState = LiftState.LS_TARGET;
-//
-//                    } else if (gamepad2.a) {
-//
-//                        liftHand = LiftHand.LH_NEAR;
-//
-//                        // Run lift
-//                        liftLeft.setTargetPosition(TARGET_NEAR);
-//                        liftRight.setTargetPosition(TARGET_NEAR);
-//                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                        liftLeft.setPower(-1.0);
-//                        liftRight.setPower(-1.0);
-//
-//                        // Set lift state to target
-//                        liftState = LiftState.LS_TARGET;
-//                    }
-//
-//                    break;
-//
-//                case LS_TARGET:
-//
-//                    // Check if the lift has fully extended
-//                    if (liftHand == LiftHand.LH_TIPPED && (Math.abs(liftLeft.getCurrentPosition() - TARGET_TIPPED)) < 10) {
-//
-//                        arm.setPosition(arm_forward);
-//
-//                        // Set state to depositing
-//                        liftState = LiftState.LS_DEPOSITING;
-//
-//                    } else if (liftHand == LiftHand.LH_BALANCED && (Math.abs(liftLeft.getCurrentPosition() - TARGET_BALANCED)) < 10) {
-//
-//                        arm.setPosition(arm_forward);
-//
-//                        // Set state to depositing
-//                        liftState = LiftState.LS_DEPOSITING;
-//
-//                    } else if (liftHand == LiftHand.LH_FAR && (Math.abs(liftLeft.getCurrentPosition() - TARGET_FAR)) < 5) {
-//
-//                        arm.setPosition(arm_forward);
-//
-//                        // Set state to depositing
-//                        liftState = LiftState.LS_DEPOSITING;
-//
-//                    } else if (liftHand == LiftHand.LH_CENTER && (Math.abs(liftLeft.getCurrentPosition() - TARGET_MIDDLE)) < 5) {
-//
-//                        arm.setPosition(arm_forward);
-//
-//                        // Set state to depositing
-//                        liftState = LiftState.LS_DEPOSITING;
-//
-//                    } else if (liftHand == LiftHand.LH_NEAR && (Math.abs(liftLeft.getCurrentPosition() - TARGET_NEAR)) < 5) {
-//
-//                        arm.setPosition(arm_forward);
-//
-//                        // Set state to depositing
-//                        liftState = LiftState.LS_DEPOSITING;
-//                    }
-//
-//                case LS_DEPOSITING:
-//                    if (gamepad2.dpad_right || gamepad2.x) {
-//
-//                        liftHand = LiftHand.LH_RETRACT;
-//
-//                        depositTimer.reset();
-//
-//                        bucket.setPosition(bucket_right);
-//
-//                        if (depositTimer.milliseconds() <= 500) {
-//
-//                            bucket.setPosition(bucket_right);
-//
-//                        } else {
-//
-//                            bucket.setPosition(bucket_down);
-//
-//                            // Set lift state to released
-//                            liftState = LiftState.LS_RELEASED;
-//                        }
-//                    }
-//
-//                    break;
-//
-//                case LS_RELEASED:
-//
-//                    if (bucket.getPosition() == bucket_down) {
-//
-//                        if ((Math.abs(liftLeft.getCurrentPosition())) < -5) {
-//
-//                            arm.setPosition(arm_backward);
-//
-//                            // Retract lift
-//                            liftLeft.setTargetPosition(-5);
-//                            liftRight.setTargetPosition(-5);
-//                            liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                            liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                            liftLeft.setPower(-1.0);
-//                            liftLeft.setPower(-1.0);
-//
-//                            // Set lift state to retracting
-//                            liftState = LiftState.LS_RETRACTING;
-//
-//                        }
-//                    }
-//
-//                    break;
-//
-//                case LS_RETRACTING:
-//                    // Check if lift is fully retracted
-//                    if ((Math.abs(liftLeft.getCurrentPosition() - 0)) < 5) {
-//
-//                        // Set lift state to empty
-//                        liftState = LiftState.LS_EXTENDING;
-//
-//                    }
-//
-//                    break;
-//            }
+            /** Lift **/
+
+            // Motor tick count is equal to 384.5
+
+            switch (liftState) {
+                case LS_EXTENDING:
+
+                    // Check if button was pressed
+                    if (gamepad2.dpad_up) {
+
+                        liftHand = LiftHand.LH_TIPPED;
+
+                        // Run lift
+                        liftLeft.setTargetPosition(TARGET_TIPPED);
+                        liftRight.setTargetPosition(TARGET_TIPPED);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(1.0);
+                        liftRight.setPower(1.0);
+
+                        arm.setPosition(arm_forward);
+
+                        liftState = LiftState.LS_TARGET;
+
+                    } else if (gamepad2.dpad_left) {
+
+                        liftHand = liftHand.LH_BALANCED;
+
+                        // Run lift
+                        liftLeft.setTargetPosition(TARGET_BALANCED);
+                        liftRight.setTargetPosition(TARGET_BALANCED);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(1.0);
+                        liftRight.setPower(-1.0);
+
+                        arm.setPosition(arm_forward);
+
+                        liftState = LiftState.LS_TARGET;
+
+                    } else if (gamepad2.y) {
+
+                        liftHand = LiftHand.LH_FAR;
+
+                        // Run lift
+                        liftLeft.setTargetPosition(TARGET_FAR);
+                        liftRight.setTargetPosition(TARGET_FAR);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(1.0);
+                        liftRight.setPower(1.0);
+
+                        arm.setPosition(arm_forward);
+
+                        liftState = LiftState.LS_TARGET;
+
+                    } else if (gamepad2.b) {
+
+                        liftHand = LiftHand.LH_CENTER;
+
+                        // Run lift
+                        liftLeft.setTargetPosition(TARGET_CENTER);
+                        liftRight.setTargetPosition(TARGET_CENTER);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(1.0);
+                        liftRight.setPower(1.0);
+
+                        arm.setPosition(arm_forward);
+
+                        liftState = LiftState.LS_TARGET;
+
+                    } else if (gamepad2.a) {
+
+                        liftHand = LiftHand.LH_NEAR;
+
+                        // Run lift
+                        liftLeft.setTargetPosition(TARGET_NEAR);
+                        liftRight.setTargetPosition(TARGET_NEAR);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(1.0);
+                        liftRight.setPower(1.0);
+
+                        arm.setPosition(arm_forward);
+
+                        // Set lift state to target
+                        liftState = LiftState.LS_TARGET;
+                    }
+
+                    break;
+
+                case LS_TARGET:
+
+                    // Check if the lift has fully extended
+                    if (liftHand == LiftHand.LH_TIPPED && (Math.abs(liftLeft.getCurrentPosition() - TARGET_TIPPED)) < 10) {
+
+                        depositTimer.reset();
+
+                        // Set state to depositing
+                        liftState = LiftState.LS_DEPOSITING;
+
+                    } else if (liftHand == LiftHand.LH_BALANCED && (Math.abs(liftLeft.getCurrentPosition() - TARGET_BALANCED)) < 10) {
+
+                        depositTimer.reset();
+
+                        // Set state to depositing
+                        liftState = LiftState.LS_DEPOSITING;
+
+                    } else if (liftHand == LiftHand.LH_FAR && (Math.abs(liftLeft.getCurrentPosition() - TARGET_FAR)) < 5) {
+
+                        depositTimer.reset();
+
+                        // Set state to depositing
+                        liftState = LiftState.LS_DEPOSITING;
+
+                    } else if (liftHand == LiftHand.LH_CENTER && (Math.abs(liftLeft.getCurrentPosition() - TARGET_MIDDLE)) < 5) {
+
+                        depositTimer.reset();
+
+                        // Set state to depositing
+                        liftState = LiftState.LS_DEPOSITING;
+
+                    } else if (liftHand == LiftHand.LH_NEAR && (Math.abs(liftLeft.getCurrentPosition() - TARGET_NEAR)) < 5) {
+
+                        depositTimer.reset();
+
+                        // Set state to depositing
+                        liftState = LiftState.LS_DEPOSITING;
+                    }
+
+                case LS_DEPOSITING:
+
+                    if (gamepad2.dpad_right || gamepad2.x) {
+
+                        liftHand = LiftHand.LH_RETRACT;
+
+                        liftState = LiftState.LS_RELEASED;
+
+                    }
+
+                    break;
+
+                case LS_RELEASED:
+
+                    if (liftHand == LiftHand.LH_RETRACT) {
+
+                        bucket.setPosition(bucket_right);
+
+                        if (depositTimer.milliseconds() > 1000) {
+
+                            bucket.setPosition(bucket_down);
+
+                            // Set lift state to released
+                            liftState = LiftState.LS_RETRACTING;
+
+                        }
+                    }
+
+                    break;
+
+                case LS_RETRACTING:
+
+                    if ((Math.abs(liftLeft.getCurrentPosition())) > 5) {
+
+                        arm.setPosition(arm_intermediate);
+
+                        // Retract lift
+                        liftLeft.setTargetPosition(0);
+                        liftRight.setTargetPosition(0);
+                        liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        liftLeft.setPower(-1.0);
+                        liftLeft.setPower(-1.0);
+
+                        // Set lift state to retracting
+                        liftState = LiftState.LS_IDLE  ;
+
+                    }
+
+                    break;
+
+                case LS_IDLE:
+
+                    // Check if lift is fully retracted
+                    if (Math.abs(liftLeft.getCurrentPosition()) < 5) {
+
+                        arm.setPosition(arm_backward);
+                        liftLeft.setPower(0);
+                        liftRight.setPower(0);
+
+                        // Set lift state to empty
+                        liftState = LiftState.LS_EXTENDING;
+
+                    }
+
+                    break;
+            }
 
             // drivetrain
 
@@ -806,21 +827,21 @@ public class RoboBlue extends LinearOpMode {
 //            }
 
 
-//            if (gamepad2.left_trigger > 0.05) {
-//
-//                c_Left.setPower(0.5 * a);
-//                c_Right.setPower(0.5 * a);
-//
-//            } else if (gamepad2.right_trigger > 0.05) {
-//
-//                c_Left.setPower(-(0.5 * b));
-//                c_Right.setPower(-(0.5 * b));
-//
-//            } else {
-//
-//                c_Left.setPower(0);
-//                c_Right.setPower(0);
-//            }
+            if (gamepad2.left_trigger > 0.05) {
+
+                c_Left.setPower(0.5 * a);
+                c_Right.setPower(0.5 * a);
+
+            } else if (gamepad2.right_trigger > 0.05) {
+
+                c_Left.setPower(-(0.5 * b));
+                c_Right.setPower(-(0.5 * b));
+
+            } else {
+
+                c_Left.setPower(0);
+                c_Right.setPower(0);
+            }
 
             // Manual Lift
 
@@ -828,35 +849,19 @@ public class RoboBlue extends LinearOpMode {
 
             if (c > 0.0) {
 
-                liftLeft.setTargetPosition(-300);
-                liftRight.setTargetPosition(-300);
+                turret.setPosition(turret_center);
+                arm.setPosition(arm_forward);
+
+                liftLeft.setTargetPosition(300);
+                liftRight.setTargetPosition(300);
                 liftLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 liftRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 liftLeft.setPower(1.0);
                 liftRight.setPower(1.0);
 
-                turret.setPosition(turret_center);
-                arm.setPosition(arm_forward);
-
-                Thread.sleep(300);
+                Thread.sleep(500);
 
                 bucket.setPosition(bucket_right);
-            }
-
-            if (gamepad1.y) {
-
-                arm.setPosition(arm_forward);
-
-                Thread.sleep(300);
-
-                bucket.setPosition(bucket_right);
-            }
-
-            if (gamepad1.a) {
-
-                bucket.setPosition(bucket_down);
-
-                arm.setPosition(arm_backward);
             }
 
             if (c < 0.0) {
@@ -878,20 +883,36 @@ public class RoboBlue extends LinearOpMode {
 
                 Thread.sleep(300);
 
-                if (liftLeft.getCurrentPosition() > -400) {
+                if (liftLeft.getCurrentPosition() < 5) {
 
                     liftLeft.setPower(0);
                     liftRight.setPower(0);
                 }
             }
 
+            if (gamepad1.y) {
+
+                arm.setPosition(arm_forward);
+
+                Thread.sleep(800);
+
+                bucket.setPosition(bucket_right);
+            }
+
+            if (gamepad1.a) {
+
+                bucket.setPosition(bucket_down);
+
+                arm.setPosition(arm_backward);
+            }
+
             /** Reset Encoders **/
 
-            if (gamepad2.right_trigger != 0) {
-
-                liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);   // set motor ticks to 0
-                liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            }
+//            if (gamepad2.right_trigger != 0) {
+//
+//                liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);   // set motor ticks to 0
+//                liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            }
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "leftFront (%.2f), leftBack (%.2f), rightFront (%.2f), rightBack (%.2f)",
